268419cbd3a68c2d56fa028f6cb8157084783be6
lautigit1 <salinaslautii@gmail.com>
2026-06-24 15:57:11 -0300

    Subo cambio en el token de sesion

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,12 +1,22 @@
 import { AppRouter } from "@/router/AppRouter";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import { Toaster } from "@/components/ui/sonner";
+import { useSessionRefresh } from "@/hooks/useSessionRefresh";
+
+function AppContent() {
+  useSessionRefresh();
+  return (
+    <>
+      <AppRouter />
+      <Toaster position="top-left" />
+    </>
+  );
+}
 
 export default function App() {
   return (
     <ThemeProvider>
-      <AppRouter />
-      <Toaster position="top-left" />
+      <AppContent />
     </ThemeProvider>
   );
 }
